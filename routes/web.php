fd304bc5d03c49523c7d97b04a65acf7291241be
samir <samirkapadwanji2602@gmail.com>
2023-04-29 11:19:48 +0530

    Add basic routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [LoginController::class, 'register']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+
+// Route::view('/login', 'login');
+// Route::view('/register', 'register');
+// Route::view('/home', 'home');
+// Route::view('/blog', 'blog');
+// Route::view('/contact', 'contact');
+
+// Route::get('/login', function () {
+//     return "login page";
+// });
+
+// Route::get('/register', function () {
+//     return "Register Page";
+// });
+
+// Route::get('/home', function (){
+//     return "Home Page";
+// });
+
+// Route::get('/blog', function (){
+//     return "Blog Page";
+// });
+
+// Route::get('/contact', function (){
+//     return "Contact Page";
+// });
